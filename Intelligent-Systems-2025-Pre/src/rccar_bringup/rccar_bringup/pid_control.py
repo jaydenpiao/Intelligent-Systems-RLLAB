caf31ac4c10c9ec65e17d6bd4ada5c7962475d69
jaydenpiao <jaydenpiao@gmail.com>
2025-10-15 06:33:22 +0900

    tweak speed n params

--- Intelligent-Systems-2025-Pre/src/rccar_bringup/rccar_bringup/pid_control.py
+++ Intelligent-Systems-2025-Pre/src/rccar_bringup/rccar_bringup/pid_control.py
@@ -85,7 +85,7 @@ class PurePursuit(Node):
         """
         self.Kp = 0.4
         self.Ki = 0.0
-        self.Kd = 0.1
+        self.Kd = 0.04
         ###################################################
         ###################################################
         self.get_logger().info(">>> Running PreProject 3")
